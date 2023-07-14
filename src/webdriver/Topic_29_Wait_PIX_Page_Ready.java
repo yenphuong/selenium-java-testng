054a00aeb369d7174ffe976bb0ad6d4856f41060
LAP14153 <lap14153@LAP14153s-MacBook-Pro.local>
2023-07-14 18:33:52 +0700

    done ready page

--- src/webdriver/Topic_29_Wait_PIX_Page_Ready.java
+++ src/webdriver/Topic_29_Wait_PIX_Page_Ready.java
@@ -1,8 +1,14 @@
 package webdriver;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,6 +19,7 @@ import org.testng.annotations.Test;
 public class Topic_29_Wait_PIX_Page_Ready {
 WebDriver driver;
 WebDriverWait explicitWait;
+Actions action;
 String projectPath = System.getProperty("user.dir");
 String osName = System.getProperty("os.name");
 
@@ -28,6 +35,7 @@ public void beforeClass() {
 	
 	driver = new FirefoxDriver();
 	explicitWait = new WebDriverWait(driver, 30);
+	action = new Actions(driver);
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 //@Test
@@ -40,7 +48,7 @@ public void TC_01_Orage_HRM_API() {
 	Assert.assertTrue(driver.findElement(By.cssSelector("div#project h1")).getText().equals("OrangeHRM REST API Documentation"));
 	
 	}
-@Test
+//@Test
 public void TC_02_Admin_NopCommerce() {
 	driver.get("https://admin-demo.nopcommerce.com");
 	
@@ -64,6 +72,67 @@ public void TC_02_Admin_NopCommerce() {
 	Assert.assertEquals(driver.getTitle(), "Your store. Login" );
 	
 	}
+//@Test
+public void TC_03_Admin_NopCommerce_Wait_Page_Ready() {
+	driver.get("https://admin-demo.nopcommerce.com");
+	
+	driver.findElement(By.cssSelector("input#Email")).clear();
+	driver.findElement(By.cssSelector("input#Email")).sendKeys("admin@yourstore.com");
+	
+	driver.findElement(By.cssSelector("input#Password")).clear();
+	driver.findElement(By.cssSelector("input#Password")).sendKeys("admin");
+	driver.findElement(By.cssSelector("button.login-button")).click();
+	
+	
+	}
+@Test
+public void TC_03_Bog_Test_Project() {
+	driver.get("https://blog.testproject.io/");
+	
+	// Hover mouse to any element at this page to page ready
+	// Use action library
+	// action luc nao cung phai perform
+	action.moveToElement(driver.findElement(By.cssSelector("h1.main-heading"))).perform();
+	 
+	Assert.assertTrue(isPageLoadedSuccess());
+	
+	String keyword = "Selenium";
+	
+	driver.findElement(By.cssSelector("section#search-2 input.search-field")).sendKeys(keyword);
+	driver.findElement(By.cssSelector("section#search-2 span.glass")).click();
+	
+	// Wait cho page heading visible
+	explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='main-heading' and text()='Search Results']")));
+	Assert.assertTrue(isPageLoadedSuccess());
+	
+	List<WebElement> listArticle = driver.findElements(By.cssSelector("h3.post-title>a"));
+
+	for (WebElement article : listArticle) {
+		Assert.assertTrue(article.getText().contains(keyword));
+	}
+
+
+}
+
+public boolean isPageLoadedSuccess() {
+	WebDriverWait explicitWait = new WebDriverWait(driver, 30);
+	JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+	ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+		@Override
+		public Boolean apply(WebDriver driver) {
+			return (Boolean) jsExecutor.executeScript("return (window.jQuery != null) && (jQuery.active === 0);");
+		}
+	};
+
+	ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+		@Override
+		public Boolean apply(WebDriver driver) {
+			return jsExecutor.executeScript("return document.readyState").toString().equals("complete");
+		}
+	};
+	return explicitWait.until(jQueryLoad) && explicitWait.until(jsLoad);
+}
+
 @AfterClass
 public void afterClass() {
 driver.quit();
